50161a76ea2b3f01a49e70d999af4b5a370d7941
ThonnyRAZA <razaheriniaina@gmail.com>
2022-09-26 15:23:04 +0400

    second_commit

--- app/src/main/java/fr/razaheri/aplication_hopital/MainActivity.kt
+++ app/src/main/java/fr/razaheri/aplication_hopital/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.TextView
 import java.util.*
 import android.app.DatePickerDialog
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +26,91 @@ class MainActivity : AppCompatActivity() {
 
             val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                 // Display Selected date in TextView
-                textView.setText("$dayOfMonth, ${monthOfYear + 1}, $year")
+
+                textView.setText("$dayOfMonth/ ${monthOfYear + 1}/ $year")
             }, year, month, day)
             dpd.show()
 
         }
 
+        // access the items of the list
+        val Intensité = resources.getStringArray(R.array.Intensite_Migraine)
+        val AINS = resources.getStringArray(R.array.AINS)
+        val Tripan = resources.getStringArray(R.array.Tripan)
+        val Traitement_de_fond = resources.getStringArray(R.array.Traitement_de_fond)
 
 
+        // access the spinner
+        var spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, Intensité)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+        spinner = findViewById<Spinner>(R.id.spnAINS)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, AINS)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+        spinner = findViewById<Spinner>(R.id.spnTripan)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, Tripan)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+        spinner = findViewById<Spinner>(R.id.spnTDF)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, Traitement_de_fond)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 }
